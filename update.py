05d1f9d822aec90fd4fabb52aec6f803c5f36232
Jack Kingston <jack.kingston@gmail.com>
2023-02-17 19:56:21 +0000

    podcast rss

--- update.py
+++ update.py
@@ -4,13 +4,17 @@ import article_to_text
 import tts
 import json
 from pydub import AudioSegment
+from feedgen.feed import FeedGenerator
 import os
 import sys
+import unicodedata
 
 if len(sys.argv) == 1:
     outdir = "./publish"
 else:
     outdir = sys.argv[1]
+
+base_url = "https://63efd2f9d13069226104c17e--heartfelt-kleicha-e5d7f4.netlify.app"
 
 if not os.path.exists(outdir):
     os.mkdir(outdir)
@@ -72,3 +76,27 @@ for article in articles:
         print("Creating", mp3_file_path)
         sound = AudioSegment.from_wav(wav_file_path)
         sound.export(mp3_file_path, format="mp3")
+
+fg = FeedGenerator()
+fg.load_extension('podcast', rss=True)
+
+fg.podcast.itunes_category('News', 'Business News')
+fg.title("Robo-Matt Levine")
+fg.link({"href": "https://www.bloomberg.com/opinion/authors/ARbTQlRLRjE/matthew-s-levine"})
+fg.description("Robo-Matt Levine")
+
+episodes = [ f for f in os.listdir(filesdir) if f.endswith(".mp3") ]
+
+for episode in episodes:
+    title = episode[:-4]
+    metadata_path = unicodedata.normalize("NFKC", f"{filesdir}/{title}.json")
+    with open(metadata_path, "r") as f:
+        metadata = json.load(f)
+    
+    fe = fg.add_entry()
+    fe.id(f"{base_url}/files/{episode}")
+    fe.title(metadata['title'])
+    fe.description(metadata['summary'])
+    fe.enclosure(f"{base_url}/files/{episode}", str(os.stat(f"{filesdir}/{episode}").st_size), "audio/mpeg")
+
+fg.rss_file(f"{outdir}/podcast.rss")
